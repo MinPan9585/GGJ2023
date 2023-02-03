8db83bc9e4d9362ae4006f334fd3b51178e4dd47
wh1te6324 <114755719+wh1te6324@users.noreply.github.com>
2023-02-03 17:57:14 +0800

    adaDAd
    阿达UYD他GU她对于他的

---
+++ Assets/script/palyercontrol1.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class palyercontrol1 : MonoBehaviour
+{
+    public float movespeed;
+    public float jumpforce;
+
+    private Rigidbody2D rb;
+    private bool facingright = true;
+    private float movedirection;
+    private bool isjumping = false;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    
+    
+    void Update()
+    {
+        Inputdeal();
+        Animation();
+        
+
+    }
+    private void FixedUpdate() {
+
+        move();
+    }
+    private void move()
+    {
+        rb.velocity = new Vector2(movedirection * movespeed, rb.velocity.y);
+        if (isjumping==true)
+        {
+            rb.AddForce(new Vector2(0f, jumpforce));
+
+        }
+        isjumping = false;
+
+    }
+    private void Inputdeal() {
+        movedirection = Input.GetAxis("Horizontal");
+
+        
+        if (Input.GetButtonDown("Jump"))
+        {
+            isjumping = true;
+        }
+
+    }
+    private void Flipcharacter() {
+
+        facingright = !facingright;
+        transform.Rotate(0f,180f,0f);
+    }
+    private void Animation() {
+        if (movedirection > 0 && !facingright)
+        {
+            Flipcharacter();
+        }
+        else if (movedirection < 0 && facingright)
+        {
+            Flipcharacter();
+        }
+       
+    }
+
+
+}
